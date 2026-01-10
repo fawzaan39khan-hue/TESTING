72e95242ae997a6f873794e008cf22ae2a8ce230
MohammedKhan3 <MohammedKhan2619@gmail.com>
2026-01-09 23:32:50 -0600

    synchronized

---
+++ Desktop/practice/Test3.java
@@ -0,0 +1,8 @@
+public class Test3{
+    public static void main(String[] args) {
+        Counter new_counter = new Counter();
+        new_counter.increment();
+        System.out.println(new_counter.getCount());
+
+    }
+}
